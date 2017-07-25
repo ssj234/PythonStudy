285e4294521a53e8d0c2349ef3818a43a1faa618
ssj234 <363432456@qq.com>
2017-07-25 23:50:06 +0800

    mofan\

--- tensorflow/mofan/tu_5_activate.py
+++ tensorflow/mofan/tu_5_activate.py
@@ -14,18 +14,21 @@ import tensorflow as tf
 import numpy as np
 
 def add_layer(inputs,in_size,out_size,activation_function=None):
-    layer_name = 'Layer %d-%d' % (in_size,out_size)
-    with tf.name_scope('layer'):
+    layer_name = 'Layer-%d-%d' % (in_size,out_size)
+    with tf.name_scope(layer_name):
         with tf.name_scope('weights'):
             Weights = tf.Variable(tf.random_normal([in_size,out_size]),name='weights')  # matrix so Upper
-        with tf.name_scope('biases'):
+            tf.summary.histogram(layer_name+'/Weights', Weights)
+	with tf.name_scope('biases'):
             biases = tf.Variable(tf.zeros([1,out_size]) + 0.1,name='biases')  # a list so Lower not zero so add 0.1
+            tf.summary.histogram(layer_name+'/biases', biases)
         with tf.name_scope('Wx_plus_b'):
-            Wx_plus_b = tf.matmul(inputs,Weights) + biases 
+            Wx_plus_b = tf.matmul(inputs,Weights) + biases
         if activation_function is None:
             outputs = Wx_plus_b
         else:
             outputs = activation_function(Wx_plus_b)
+	tf.summary.histogram(layer_name+'/outputs', outputs)
         return outputs
 
 # define the train data
@@ -43,6 +46,7 @@ prediction = add_layer(l1,10,1,activation_function=None)
 
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]),name='loss')
+    tf.summary.scalar('loss',loss)    
 
 with tf.name_scope('train'):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss) # learning_rate less than 1
@@ -50,15 +54,19 @@ with tf.name_scope('train'):
 init = tf.initialize_all_variables()
 sess = tf.Session()
 #writer = tf.train.SummaryWriter('logs/',sess.graph)
+merged = tf.summary.merge_all()
 writer = tf.summary.FileWriter('logs/',sess.graph)
 sess.run(init)
 
 for i in range(1000):
     sess.run(train_step,feed_dict={xs:x_data,ys:y_data}) # feed_dict是为了方便小批量训练
     if i % 50 :
-        print sess.run(loss,feed_dict={xs:x_data,ys:y_data})
-        print sess.run(prediction,feed_dict={xs:np.array([[0.34]])})
+        #print sess.run(loss,feed_dict={xs:x_data,ys:y_data})
+        #print sess.run(prediction,feed_dict={xs:np.array([[0.34]])})
         #prinn sess.run()
+	result = sess.run(merged,
+			 feed_dict={xs:x_data,ys:y_data})
+        writer.add_summary(result,i)
 
 
 
