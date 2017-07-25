e29861d9e7e0375f5b57b0a0866c36456a12a310
shisj <tk1143413@163.com>
2017-07-25 22:52:46 +0800

    0725

--- tensorflow/mofan/tu_5_activate.py
+++ tensorflow/mofan/tu_5_activate.py
@@ -16,19 +16,17 @@ import numpy as np
 def add_layer(inputs,in_size,out_size,activation_function=None):
     layer_name = 'Layer %d-%d' % (in_size,out_size)
     with tf.name_scope('layer'):
-        with tf.name_scope('weights'):  
-    #layer_name = 'Layer %d-%d' % (in_size,out_size)
-    #print layer_name
-    Weights = tf.Variable(tf.random_normal([in_size,out_size]),name='weights')  # matrix so Upper
-  with tf.name_scope('biases'):
-     biases = tf.Variable(tf.zeros([1,out_size]) + 0.1,name='biases')  # a list so Lower not zero so add 0.1
-  with tf.name_scope('Wx_plus_b'):
-    Wx_plus_b = tf.matmul(inputs,Weights) + biases 
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
-    return outputs
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size,out_size]),name='weights')  # matrix so Upper
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1,out_size]) + 0.1,name='biases')  # a list so Lower not zero so add 0.1
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.matmul(inputs,Weights) + biases 
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)
+        return outputs
 
 # define the train data
 x_data = np.linspace(-1,1,300)[:,np.newaxis]
@@ -43,9 +41,11 @@ with tf.name_scope('inputs'):
 l1 = add_layer(xs,1,10,activation_function=tf.nn.relu)
 prediction = add_layer(l1,10,1,activation_function=None)
 
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]))
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]),name='loss')
 
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss) # learning_rate less than 1
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss) # learning_rate less than 1
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
