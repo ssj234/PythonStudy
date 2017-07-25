d57a4f8637600f44f8d9173952173dfdf2a1a9af
ssj234 <363432456@qq.com>
2017-07-25 22:43:38 +0800

    0725

--- tensorflow/mofan/tu_5_activate.py
+++ tensorflow/mofan/tu_5_activate.py
@@ -3,13 +3,26 @@
 # why we need activation?
 # resolve the problem those we cannot use linear
 # add layer1 and layer2 ,each layer have weight and biases
+# tensorboard --logdir='logs/'
+# tf.histogram_summary(layer_name+'/weights',Weights)
+# tf.histogram_summary(layer_name+'/biases',biases)
+# tf.scalar_summary('loss',loss)
+# merged = tf.merge_all_summaries() # write befor filewriter
+# writer.add_summary(result,i) # in train loop
 
 import tensorflow as tf
 import numpy as np
 
 def add_layer(inputs,in_size,out_size,activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size,out_size]))  # matrix so Upper
-    biases = tf.Variable(tf.zeros([1,out_size]) + 0.1)  # a list so Lower not zero so add 0.1
+    layer_name = 'Layer %d-%d' % (in_size,out_size)
+    with tf.name_scope('layer'):
+        with tf.name_scope('weights'):  
+    #layer_name = 'Layer %d-%d' % (in_size,out_size)
+    #print layer_name
+    Weights = tf.Variable(tf.random_normal([in_size,out_size]),name='weights')  # matrix so Upper
+  with tf.name_scope('biases'):
+     biases = tf.Variable(tf.zeros([1,out_size]) + 0.1,name='biases')  # a list so Lower not zero so add 0.1
+  with tf.name_scope('Wx_plus_b'):
     Wx_plus_b = tf.matmul(inputs,Weights) + biases 
     if activation_function is None:
         outputs = Wx_plus_b
@@ -23,8 +36,9 @@ noise = np.random.normal(0,0.05,x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
 #
-xs = tf.placeholder(tf.float32,[None,1]) #None:多少行都可以
-ys = tf.placeholder(tf.float32,[None,1]) 
+with tf.name_scope('inputs'):
+    xs = tf.placeholder(tf.float32,[None,1],name='x_input') #None:多少行都可以
+    ys = tf.placeholder(tf.float32,[None,1],name='y_input') 
 
 l1 = add_layer(xs,1,10,activation_function=tf.nn.relu)
 prediction = add_layer(l1,10,1,activation_function=None)
@@ -35,6 +49,8 @@ train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss) # learning_ra
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
+#writer = tf.train.SummaryWriter('logs/',sess.graph)
+writer = tf.summary.FileWriter('logs/',sess.graph)
 sess.run(init)
 
 for i in range(1000):
@@ -42,7 +58,7 @@ for i in range(1000):
     if i % 50 :
         print sess.run(loss,feed_dict={xs:x_data,ys:y_data})
         print sess.run(prediction,feed_dict={xs:np.array([[0.34]])})
-
+        #prinn sess.run()
 
 
 
